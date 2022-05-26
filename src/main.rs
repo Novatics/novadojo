7ed828239f12019bf0e6546f1a7a9110cf54fcd7
gsanto <espiritosanto.gabriel@gmail.com>
2022-05-26 19:15:33 -0300

    tried to remove !vec

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 #[cfg(test)]
 mod test;
-
 fn main() {
-  let s: Vec<String> = vec!["blue".to_string()];
-  let g: Vec<String> = vec!["blue".to_string()];
+  let s: &[&str] = ["blue"];
+  let g: &[&str] = ["blue"];
   evaluate(s, g);
 }
-
-fn evaluate(secret: Vec<String>, guess: Vec<String>) -> Vec<u8> {
+fn evaluate(secret: &[&str], guess: &[&str]) -> Vec<u8> {
   //iterar
   let mut count_match: u8 = 0;
   let mut count_error: u8 = 0;
 
   for (i, x) in guess.iter().enumerate() {
-    // count_match valor no lugar certo 
+    // count_match valor no lugar certo
     if secret[i] == *x {
       count_match += 1;
     }
@@ -22,13 +20,7 @@ fn evaluate(secret: Vec<String>, guess: Vec<String>) -> Vec<u8> {
         count_error += 1;
       }
     }
-
-    // if (secret.contains(&x)) {
-    //   count_match += 1;
-    // }
   }
-  println!("Secret: {:?}", secret);
-  // println!("Guess: {:?}", guess);
 
   return vec![count_match, count_error];
 }
--- src/test.rs
+++ src/test.rs
@@ -1,33 +1,37 @@
 use super::*;
-
 #[test]
 fn it_works() {
-    let v1 = vec!["blue".to_string()];
-    let v2 =  vec!["red".to_string()];
+    let v1 = ["blue"];
+    let v2 =  ["red"];
     let result = evaluate(v1, v2);
     assert_eq!(result, [0, 0]);
 }
-
 #[test]
 fn it_work2() {
-    let v1 = vec!["blue".to_string(),];
-    let v2 =  vec!["blue".to_string(),];
+    let v1 = ["blue"];
+    let v2 =  ["blue"];
     let result = evaluate(v1, v2);
     assert_eq!(result, [1, 0]);
 }
-
 #[test]
 fn it_work3() {
-    let v1 = vec!["red".to_string(),"yellow".to_string()];
-    let v2 =  vec!["blue".to_string(),"red".to_string()];
+    let v1 = ["red","yellow"];
+    let v2 =  ["blue","red"];
     let result = evaluate(v1, v2);
     assert_eq!(result, [0, 1]);
 }
-
 #[test]
 fn check_with_two() {
-    let v1 = vec!["blue".to_string(),"blue".to_string()];
-    let v2 =  vec!["blue".to_string()];
+    let v1 = ["blue","blue"];
+    let v2 =  ["blue","red"];
     let result = evaluate(v1, v2);
-    assert_eq!(result, [2, 0]);
+    assert_eq!(result, [1, 0]);
 }
+#[test]
+fn check_with_two_reversed() {
+    let v1 = ["blue","red"];
+    let v2 =  ["blue","blue"];
+    let result = evaluate(v1, v2);
+    assert_eq!(result, [1, 1]);
+}
+
